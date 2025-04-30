86a3f24f139487b0d86ba845920ffac979850d97
Pranto Deb <rajprit0m@outlook.com>
2025-05-01 02:55:14 +0600

    [Redux] : Connected the theme reducer to the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from "./features/theme/themeSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// Persist config for theme
+const themePersistConfig = {
+  key: "theme",
+  storage,
+};
+
+const persistedThemeReducer = persistReducer(themePersistConfig, themeReducer);
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    theme: persistedThemeReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
