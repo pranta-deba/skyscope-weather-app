af98178b983626b47497f361fc0a93386311c86a
Pranto Deb <rajprit0m@outlook.com>
2025-05-01 06:06:16 +0600

    [UI]: background class based weather condition

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { addToHistory } from "./redux/features/searchHistory/searchHistorySlice"
 import ErrorMessage from "./components/ErrorMessage";
 import Welcome from "./components/Welcome";
 import WeatherInfo from "./components/WeatherInfo";
+import { getWeatherBackgroundClass } from "./utils/formatters";
 
 const App = () => {
   const mode = useAppSelector(selectedTheme);
@@ -35,18 +36,20 @@ const App = () => {
     }
   }, [data, dispatch]);
 
+  //*  background class based weather condition
+  const backgroundClass = data
+    ? getWeatherBackgroundClass(data.condition)
+    : "bg-gradient-to-br from-blue-100 to-blue-200";
+
   return (
     <div
-      className={`min-h-screen  transition-colors duration-500 dark:bg-gray-800 dark:from-gray-800 dark:to-gray-900`}
+      className={`min-h-screen ${backgroundClass} transition-colors duration-500 dark:bg-gray-800 dark:from-gray-800 dark:to-gray-900`}
     >
       <div className="min-h-screen backdrop-blur-sm dark:bg-black/30 transition-colors duration-500">
         <Header />
 
-        <main className="max-w-6xl mx-auto px-4 py-6">
-          <div className="mb-6">
-            <SearchBar />
-          </div>
-
+        <main className="min-h-[calc(100vh-119.944px)] max-w-6xl mx-auto px-4 py-6 space-y-5">
+          <SearchBar />
           <ErrorMessage />
           <Loading />
           <Welcome />
